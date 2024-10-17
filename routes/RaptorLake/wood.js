45252503086566f64483d10c4dfcda8e4c91a156
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2024-10-17 13:54:15 +0700

    Refactor server startup and routing configuration, update skill item ID and quantity in wood.js

--- routes/RaptorLake/wood.js
+++ routes/RaptorLake/wood.js
@@ -1,5 +1,5 @@
 async function woodRaptor() {
-  return await fetch("https://web.idle-mmo.com/api/skills/start?expires=1729231711&signature=fc40c94d312cabbf0501c33f9d1341f5038855f94a68b197d1ed28f1d02d3a67", {
+  return await fetch("https://web.idle-mmo.com/api/skills/start?expires=1729234414&signature=a354a2bf2de29eef17c7e50e8a533fbefaa76939907cdd771db467a5eed40d12", {
     "headers": {
       "accept": "application/json",
       "accept-language": "id,en-US;q=0.9,en;q=0.8",
@@ -12,11 +12,11 @@ async function woodRaptor() {
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
-      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; gawain_lancelot=c2b585bif73c; dropdown_skills=true; XSRF-TOKEN=eyJpdiI6IkFpL0FjQWVJWUtyR0p6dDhseHBZU2c9PSIsInZhbHVlIjoicFhsdEd6UUhFRGQxd3dHV1Rtaktxdi9jREU3L0RWR0ZIMkltWHRxTjlSZUNLZzNvN2xnZk1HR2UwY29lVGsxejJ6U2ZoRUkyVzZ4WjJLcjZOeGR4WXkzc0Zma1pLeFNkcHpzcFg4OUdKZTU0NkpRcHZDSEYzdzlWTERHeERoT3ciLCJtYWMiOiJlMmVjZjFmMDEwODY0NDRmYmI5OGUwZWEwODkyZTA2OTEwYzY1NjIzYTgwN2M1OTM4YTQzYmYzMjNmNjk5YTUzIiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6IkFqbkkrT0FYamtYclcxbkdHbTVTVmc9PSIsInZhbHVlIjoiWGNBRmFtZllBZ3I3aUE1Uzh0VDU1bWRFZGJxcjRnZlZJUldYQkxwaDBJdHI3Q2I3U0xhT0lJL0Z1U3hKMUdYcmk3SnBOamx2WkRySGhZVlVBMU5aRGMvTFZCVnE5czF2TlpnTHk1eG9QajdkeUlEaFNUbi9EczZmanR2VkdpQ2siLCJtYWMiOiJlN2Y1YWU1ODkwYWUyNjA5M2VhYjU5MGVjNjFmMmUwZDRkNWVjYzQ0NWIyNGVmYjMwYzg0NzNlYjM2OTYxYzM3IiwidGFnIjoiIn0%3D",
-      "Referer": "https://web.idle-mmo.com/@RaptorLake",
+      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; gawain_lancelot=c2b585bif73c; dropdown_skills=true; XSRF-TOKEN=eyJpdiI6IlcvNGFTYjl5ZmFoNGk2czJpMWRaZUE9PSIsInZhbHVlIjoid1dKTkRUU1NxYmt0UGZkVVUvb3dlYzFaZGlPdE5qZEdjTGg3RFVjRHdqVmczd1pPTFFTWWxiTlpLTGhCQnhVemVHMDFEaUJCUGU2b3k1K0JxbEZzVVJqSFEvdzBPVGVjc1NEN3FpOEg4dDZwSXNTazl0MzVTWjlhN0V5QnQ1QTUiLCJtYWMiOiIyZmY3MTIzZTlhNWYzM2JhMTI2MWMyYWIzYWU5MTcwNWY5YjRmNzllNjI1ZmFmYzhiMzMyZjk5MWFhMzBmZjBlIiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6InhGV0dnM0lXOXdZQ1d1WDVydktrR0E9PSIsInZhbHVlIjoicytUUlRsbFNxT29mKzgvNnFsOThZc2dWbHNjMmFnekhtT3ZwbWgxMjhONEJlL29QK3M5aDdNdHJaOU9HaXoxdkptcDZVS0lMV1ZYcHFXdWJVSmJxbURQZm44eUtBQnl3UWsxd1IwMzlSeW9tcWRPMkZ6Z2ErblA5SUdnNFkxZ3MiLCJtYWMiOiI4OGJkOWExNzE5ZGZkMzAyZjI0ZmUxYjVmNDQ3OWZhNzYzNDExZDU2MTc5MThkYzFmMTE5ZmMyMTcwMTBhN2M0IiwidGFnIjoiIn0%3D",
+      "Referer": "https://web.idle-mmo.com/skills/view/woodcutting",
       "Referrer-Policy": "strict-origin-when-cross-origin"
     },
-    "body": "{\"skill_item_id\":1,\"quantity\":440,\"ts2mic5ytx\":\"VFFZ\",\"qty6bx4peh\":\"VFNY\",\"v\":\"1.0.0.1\"}",
+    "body": "{\"skill_item_id\":2,\"quantity\":352,\"ts2mic5ytx\":\"U1Zd\",\"qty6bx4peh\":\"VFBd\",\"v\":\"1.0.0.1\"}",
     "method": "POST"
   });
 }
