1e6e84c1a1cf3d01d5222e48f231508c102ac419
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2024-10-17 07:44:42 +0700

    Refactor server startup and routing configuration, update skill item ID and quantity

--- routes/HackerInside/expedition.js
+++ routes/HackerInside/expedition.js
@@ -1,5 +1,5 @@
 async function expeditionHacker() {
-  return await fetch("https://web.idle-mmo.com/api/dungeon/start/1?expires=1729149152&signature=12390a9145bee36ea70bfefcf8f4e26fc560ec632eb4e693e84a4515adeef7dd", {
+  return await fetch("https://web.idle-mmo.com/api/dungeon/start/1?expires=1729212081&signature=4d684d840d81573f815819c1876dd70be13634b621bdcd86506c833e556d86be", {
     "headers": {
       "accept": "application/json",
       "accept-language": "id,en-US;q=0.9,en;q=0.8",
@@ -12,11 +12,11 @@ async function expeditionHacker() {
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
-      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; dropdown_skills=true; gawain_lancelot=c2b585bif73c; XSRF-TOKEN=eyJpdiI6IjRZQzAySE9ycERBNnVwSkpMUjk1M1E9PSIsInZhbHVlIjoieUVaeWJKek5vY3BNcDJCcEIrYjFtVi81SXd2bTBoTUdscVZUMHVCd051WlhCTWdGQkFKOXl5alAyYzhWQUJBOGc1RFZ2T3l2VXJ6T1NFMGVBWXNUOWgzTzhPWmxUZEFVbFhpbnl2eHZlOTU5V3RQcDM1bmtZTzM2TG82WmQzc2YiLCJtYWMiOiI0MjYzZWFlZGVlODQ2OWVkY2QyNjA5MTIwNDkzMTM2YmNkZjQyYTE3YWQyMjljZmE0NzhiZGM4YzNkYjY1OGU1IiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6IlhJWEdSR0NYLzN6WHVkTFJ6MGMxNVE9PSIsInZhbHVlIjoibUoyZ241cUprdXJrMC9nYVhlNm9DeEdOR3dUVWZ2YkNhRDZ5cS9IZllFeUppVzI1c2c3U3FJc3VTRVdaWGJ0azY5dUVRK1U0ZjJCNTU2MnBqRDdhbUp1M3FWTURPckw3bzI0ZzRYWkdISkhmemZpMVB4ZUlzKzE1cnFkcExHRmQiLCJtYWMiOiIyMzdjOGU2Mzk1NTY5Y2Y3Mjk3MjExODdhY2IwODZmNjU0ZDRlODQxMWI0YjI1OGQwYmM1ZWY5MGUyZjJjYzlmIiwidGFnIjoiIn0%3D",
+      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; dropdown_skills=true; gawain_lancelot=c2b585bif73c; XSRF-TOKEN=eyJpdiI6InhNYjdtMmRoWExWUTd2U1RFQjVvMVE9PSIsInZhbHVlIjoiSEpwdlNMQTJZNUdDVUk4Lzc5bFdZVisxNW1nbTZ3cWlDK0c0NHIyRjBQTW9RUXRUMVB5SDA2c1NXejFmQVByRkZmUFFOWWVLNkNlbTdDUU92cWlleWdEaEV0aG01NzZ2SWR1ZXVNMDVCbUpxRWpvNFBqbm9CTjROdWh5L0RMUzMiLCJtYWMiOiIyY2VjYThkZWM5M2I2ZjViMGZlY2FiODhiZjJjNTQxOWE0ZGZiNWNlZmY3NTNhMTFjZTI4OWQ2ZGM2YTEwNGQyIiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6IlRZbkdhdDFaUFZlcTNCZnVVUnFrU1E9PSIsInZhbHVlIjoiVlV2aWZvWk82aXpENVdjK2l4VlhlM292eElncXNDdUptL3lnK1RZRU8vNmdLWjhuQW1ycUYrQi9DN25ObmFJcy96S1hDZUdBQWRUUVA4T0MwcHdLaHpLc1Y1V1dCMURnaXRwQWg2MFFSd3VlNUNZd0VOaThvWkg5ZHNuVytLdjUiLCJtYWMiOiI4Mzc5NTExMjczM2M3ZTZiYjY1YTA4MDFiNDM4MGZiMDZlYTgyY2E3ZWY3ZDYxMjE2YTg3MzM3YTMzNDQyNGRlIiwidGFnIjoiIn0%3D",
       "Referer": "https://web.idle-mmo.com/battle",
       "Referrer-Policy": "strict-origin-when-cross-origin"
     },
-    "body": "{\"ts2mic5ytx\":\"UlBQ\",\"qty6bx4peh\":\"U1Zd\",\"v\":\"1.0.0.1\"}",
+    "body": "{\"ts2mic5ytx\":\"UFJY\",\"qty6bx4peh\":\"UFZd\",\"v\":\"1.0.0.1\"}",
     "method": "POST"
   });
 }
