f2bac57e9d7f85262971e587f7a6ad3ad67103ca
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2024-10-18 14:10:39 +0700

    Refactor server startup and routing configuration, update skill item ID and quantity in switch.js

--- routes/RaptorLake/wood.js
+++ routes/RaptorLake/wood.js
@@ -1,9 +1,9 @@
 async function woodRaptor() {
-  return await fetch("https://web.idle-mmo.com/api/skills/start?expires=1729305961&signature=e75994bd991b3b66d79c44f59c5e7b8dcd201dd8e7bfc73fee5cd913fbfb12df", {
+  return await fetch("https://web.idle-mmo.com/api/skills/start?expires=1729321809&signature=c0615d31226d170c3808dbd433f5e0de0231e7a2e67cde5cde56d2f8f08f7dd0", {
     "headers": {
       "accept": "application/json",
       "accept-language": "id,en-US;q=0.9,en;q=0.8",
-      "authorization": "Bearer zPJOb3TsnbVqMLW9kdnMu7Xld4GHIJrb1u4uhfRul8FuIGv5HNW6Lwg4hUkI",
+      "authorization": "Bearer PZY1L4PAodoFTac0GtcGhsGdPpWvHtHksE4RTnXz7RYFtUY2F9ob69p0iSa8",
       "content-type": "application/json",
       "priority": "u=1, i",
       "sec-ch-ua": "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
@@ -12,11 +12,11 @@ async function woodRaptor() {
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
-      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; gawain_lancelot=c2b585bif73c; dropdown_skills=true; XSRF-TOKEN=eyJpdiI6IjJ2bTJGeUVOOU1VdUJVaXcwVW9wZHc9PSIsInZhbHVlIjoiT3M2NnZwZzE1SDgwcjV5NmhCWnhIRlR5bnNGOTJSUlRtc3F5TXZsUGJYdVR0ZHUvMXVjaEZoMG8zTmVhK2J0blZ0bm12SU03dndxNmNkeVdYQjhqZS8rZHFleC90Rk1kcnAvYnpHSERkV3dRY3NIV2pJcmsyVmFvWEtjK3hVZUYiLCJtYWMiOiIzZDg5NDlmYTA4YjIwNjkzNTUwMWRlMGYyYjRmNzNiNjlmYWY5ZGI5YjJlN2Q2ODI1YzgzMGUxYzg5ZTA4Y2RkIiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6IlhjNXZwL2o5Qi91eU9DQ2JuVHduSWc9PSIsInZhbHVlIjoiWXFYRVlRbVozUjNlL1RaeEJ5K1BsQkVnek5xS2g2OFFOcjJEK1Y3Zm5zYkZmV3NBcWhxVWIyNUdIRHNhcDljZUdOR2U2YWZ4V3NkQVUvNC9KMy9yV2dhamJFbXR0K0ZkMEVzVXRLZkZ4cEhnR0k0ZmRyeW9yS3pIdjRGcVRqS2giLCJtYWMiOiIzNTlkODc2Nzk2MmEyNzM4NmJkNDVmNGQ2OTZhMWE2MDViZTQ1ZWE0YmQyNTgxZWVkMjMzNGVhMjM5NWI4YzhiIiwidGFnIjoiIn0%3D",
-      "Referer": "https://web.idle-mmo.com/skills/view/woodcutting",
+      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; gawain_lancelot=c2b585bif73c; dropdown_skills=true; XSRF-TOKEN=eyJpdiI6InU1NmFzNnZpVS9HQTBXTGZMZkxQT0E9PSIsInZhbHVlIjoiSnR1RHNoNWJla3k1TjRlUis2dUZXOVQ0WUo0enBjZDloSUpzM293ZmRRNXNYNTRRVUNUcGhhYlkwa0lGT2JBK2lxckpURS93TVM0YmRtZTMrUXlBY0RKVWtwTHBFdEJYeHgzTzIzcDNLVW1OWlZrUDgvM1RLWmxuVWVnNk14dDIiLCJtYWMiOiJiZTg1N2RiNTQ1NWM2ZTM1ODM4MGQ1NDJlYjk4NWE3MWFmODJhYjNjODk0MDNhZTQzZTgxODU0MjY3ZDczYzMyIiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6Ilp2Um1oT2haT1c0aDg1anE5TWR5S0E9PSIsInZhbHVlIjoiaGNXZVhGTlJma1FMcXdnZGx5VDVZSXpFREpPWjFSM0VmWmYvaXhSL09RenBTQXFwU2pvNTFUYStROHNmTU1ETW1PUDNHNkJGZ2tVb2cydFU0ZnhQM3c4UVhQODY2b3dpUmV2SmZFVkpzOEJlUlU1dm40L2NQWWwxVnNMeFI1YVUiLCJtYWMiOiI1MzUzNmUyMjI5Nzg3ZGFkYjdmN2M3NTdkYTEwZDg0NmI2MjIxMDA5YTYyOTJhMzI3Y2FjYTg4YTNlNTA1MjQ0IiwidGFnIjoiIn0%3D",
+      "Referer": "https://web.idle-mmo.com/@RaptorLake",
       "Referrer-Policy": "strict-origin-when-cross-origin"
     },
-    "body": "{\"skill_item_id\":6,\"quantity\":146,\"ts2mic5ytx\":\"VVVb\",\"qty6bx4peh\":\"VFlc\",\"v\":\"1.0.0.1\"}",
+    "body": "{\"skill_item_id\":6,\"quantity\":146,\"ts2mic5ytx\":\"VVJR\",\"qty6bx4peh\":\"VFda\",\"v\":\"1.0.0.1\"}",
     "method": "POST"
   });
 }
--- routes/RyzenRenoir/mining.js
+++ routes/RyzenRenoir/mining.js
@@ -1,9 +1,9 @@
 async function miningRyzen() {
-  return await fetch("https://web.idle-mmo.com/api/skills/start?expires=1729306067&signature=6b2ce47732eb50fd2741771373c96ae9851f510c74699c9ec1e5117efdcc5cb3", {
+  return await fetch("https://web.idle-mmo.com/api/skills/start?expires=1729321689&signature=a10d9cecbda09c6ee93ad5e157b9697283d90b2ea586cffff7f36d4209a50bd9", {
     "headers": {
       "accept": "application/json",
       "accept-language": "id,en-US;q=0.9,en;q=0.8",
-      "authorization": "Bearer zPJOb3TsnbVqMLW9kdnMu7Xld4GHIJrb1u4uhfRul8FuIGv5HNW6Lwg4hUkI",
+      "authorization": "Bearer PZY1L4PAodoFTac0GtcGhsGdPpWvHtHksE4RTnXz7RYFtUY2F9ob69p0iSa8",
       "content-type": "application/json",
       "priority": "u=1, i",
       "sec-ch-ua": "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
@@ -12,11 +12,11 @@ async function miningRyzen() {
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
-      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; gawain_lancelot=c2b585bif73c; dropdown_skills=true; XSRF-TOKEN=eyJpdiI6ImpaQm83VG1HMEJvR2JaUHY1ckxkdUE9PSIsInZhbHVlIjoiQkJVSU1odkJ0TUlNTi9EK0JISDIya2R2d3hneXdqbWhzemh5enlOa0tuNnJoaWx0NmFlSzMweDRXM3VkUVNCZHl5RXpWRnN4aTRYWThxTGpQaE1jUUp4ZDByRkNwYmlmVlk4cmh5M1BUU3hUNXhscjU5dG5xTEE5U2ZWTFVya3MiLCJtYWMiOiI2YjBjOWYwMTA0YjE4MzNmMWU3OGZlMDE2MDYwOWI5ZTY1MWQyMmZiMmQ1MjI5YmFkNjhkOWU2YjQ4M2Y5NGJhIiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6IkZ4VkpLc2VEVFRvZEJlaGZZMllBQUE9PSIsInZhbHVlIjoiUUlQS0daV29DNVEvTmd4Yk1YMnFBZXR4RmFIOWl5aElad2F0V3hsQURIdzhlR3J3eFNUVkoyZDlIbFBaZE15VE50Z0QwTmJhRjN2cnExMVgraVVOajRodUt5SmpQUkxoR3B1UmJsaEcycjNvaWhnMElQSTZXL2J3NzNFVkNjZDQiLCJtYWMiOiI5OTIyNmQ3YTgwMWYxNjNhMWU4NDBkNTUwZjE0NzU4ZjJkMzk1NGE3Y2Y0YzBkOTg0YjMyNWRkYjE1Y2JjNzk2IiwidGFnIjoiIn0%3D",
+      "cookie": "dismissed_announcements=3; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6Ikp1c29CVjVsV1ZQVDhmTS9NTnlXdGc9PSIsInZhbHVlIjoiUGcyRnpXRmJmZDJ3bVdQOTJkN2VlUEJtemZ3SlNRNkJDTGc2MzhJMDc1bGprR3RkZG9zUENqWWhQa3FVdVA4My9xdmdrV0hscjA4M2xRTU9yU1BnTmIvVDdxRjBPTEg5WERsZnpVREFoZ2dCRytCYXdrWHM1allmYVRqTG83TUlqeFM1dm91SVFUNHJURnpSOXBrcFRqbGlESVM5bkNVYzBUdzJxUGJuWnFRaGtqNWZQdGFoS2lqa3dmNnZSUmllNnBvU3Rtbm9XZDI4QzlTZUlBdnZ6d3R0eit6SUFkR1NnYjdOdnZyT3J6RT0iLCJtYWMiOiI5NDI0ZjY0ODVlMzc5NTM4YzMwNzAwNGIxNGQ0MTU1OGIzZTIwNjAyM2I2ZGJhNWNhZmVkOTM4YjM4ZDk0ZDgxIiwidGFnIjoiIn0%3D; gawain_lancelot=c2b585bif73c; dropdown_skills=true; XSRF-TOKEN=eyJpdiI6Imo3akI2ejc5Z2oyVVlubUFaZGk1K2c9PSIsInZhbHVlIjoicTR0QVJiNW9heEJ4VE9KR09UUzhJeEk0elR2eDdMMDI4MWRpcWEzTitWSDByZ09mbHkwemV0SENGdVl2RER6Mk5DUGJobm1XcnEvSDllT2xCVlRVOWZxSDdUQmJsRGNwYjNQWnNVa2FSV2ZXYXpRRXFIVlpKSUx1UmJoRThISnkiLCJtYWMiOiJlNDlkZDVhZDg3YmE1OTIyODg1ODY1M2VmMWI4Y2E3YmNlYzE2NGU2Y2EyNjYxMDI1ZDY4Yjk2NzRlZGZlNDA1IiwidGFnIjoiIn0%3D; idlemmo_session=eyJpdiI6Ik1xZDgrWlFmSEppa09jUWplVzZUc2c9PSIsInZhbHVlIjoiUlpIa0R6dTZ0L0RaQ2J2MzhUMWJGYXNSUUg3ZmthTFJvQ2tUMWdFZEY3SUxCaklpcVJUUlFNTnZDZWl0KzhUZDBrb2R3MGRWNDNLeWtQWHdReDhkb2RENXNuVUhwZHd4VHBuSU1YaHd2U0hxL0dZMkk5UVhiN2NvMnc5bEdCNVgiLCJtYWMiOiJiMjgzNGZkZjQ5ZTM3YTcyNDFiOWY3YWU2NjQ4ZjkzZmExYzYzNWVjN2QxMTQxYjdmNzgyMTQzZmZkYzMwNDhhIiwidGFnIjoiIn0%3D",
       "Referer": "https://web.idle-mmo.com/@RyzenRenoir",
       "Referrer-Policy": "strict-origin-when-cross-origin"
     },
-    "body": "{\"skill_item_id\":17,\"quantity\":161,\"ts2mic5ytx\":\"VVJZ\",\"qty6bx4peh\":\"U1Ja\",\"v\":\"1.0.0.1\"}",
+    "body": "{\"skill_item_id\":17,\"quantity\":161,\"ts2mic5ytx\":\"VVJd\",\"qty6bx4peh\":\"U1JR\",\"v\":\"1.0.0.1\"}",
     "method": "POST"
   });
 }
